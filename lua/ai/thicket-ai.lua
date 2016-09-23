579da54195a59f6fbc147f6095bf44f50d36fb4d
Vergil <zjw917329684@gmail.com>
2016-09-23 23:06:55 +0800

    更改曹丕放逐好感度仇恨值增减

--- lua/ai/thicket-ai.lua
+++ lua/ai/thicket-ai.lua
@@ -110,7 +110,7 @@ sgs.ai_playerchosen_intention.songwei = -50
 sgs.ai_playerchosen_intention.fangzhu = function(self, from, to)
 	if hasManjuanEffect(to) then sgs.updateIntention(from, to, 80) end
 	local intention = 80 / math.max(from:getLostHp(), 1)
-	if not self:toTurnOver(to, from:getLostHp()) then intention = -intention end
+	if not self:toTurnOver(to, from:getLostHp()) then intention = -(intention + 20) end
 	if from:getLostHp() < 3 then
 		sgs.updateIntention(from, to, intention)
 	else
