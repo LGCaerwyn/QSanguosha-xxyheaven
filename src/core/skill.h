380478b36ddfcf15bd9a5f693a2e7bdfcb3024dc
Vergil <zjw917329684@gmail.com>
2016-10-05 22:41:51 +0800

    删除无用的spconvertskill

--- src/core/skill.h
+++ src/core/skill.h
@@ -229,21 +229,6 @@ public:
     virtual void onGameStart(ServerPlayer *player) const = 0;
 };
 
-class SPConvertSkill : public GameStartSkill
-{
-    Q_OBJECT
-
-public:
-    SPConvertSkill(const QString &from, const QString &to);
-
-    virtual bool triggerable(const ServerPlayer *target) const;
-    virtual void onGameStart(ServerPlayer *player) const;
-
-private:
-    QString from, to;
-    QStringList to_list;
-};
-
 class ProhibitSkill : public Skill
 {
     Q_OBJECT
